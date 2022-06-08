8c802e8e5fc3539a551002e57b2172439a179d1c
hieuntcasso <94815095+hieuntcasso@users.noreply.github.com>
2022-06-08 23:10:23 +0700

    init commit

---
+++ resolvers.js
@@ -0,0 +1,10 @@
+const db = require('./db')
+
+const Query = {
+    
+    greeting: () => {
+        return "hello from  TutorialsPoint !!!"
+    },
+    students: () => db.students.list()
+}
+module.exports = { Query }
